17e74f6d82168581f69d9638e347738771d27403
Winston Chan <winstonchan05@gmail.com>
2022-06-10 02:38:44 -0700

    deplotment and other edits

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ renderer.setClearColor(0x3670c7, 1);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
-const geometry = new THREE.OctahedronGeometry(20, 1);
+const geometry = new THREE.TorusGeometry(20, 5, 10, 70);
 const material = new THREE.MeshBasicMaterial({
   color: 0xffffff,
   wireframe: true,
@@ -32,7 +32,17 @@ const material = new THREE.MeshBasicMaterial({
 const center = new THREE.Mesh(geometry, material);
 
 scene.add(center);
+camera.position.setX(-20);
 center.position.setZ(-50);
+
+const geometryCube = new THREE.OctahedronGeometry(10, 2);
+const materialCube = new THREE.MeshBasicMaterial({
+  color: 0xffffff,
+  wireframe: true,
+});
+const centerCube = new THREE.Mesh(geometryCube, materialCube);
+scene.add(centerCube);
+centerCube.position.setZ(-50);
 
 function addstars() {
   const geometry = new THREE.OctahedronGeometry(200, 1);
@@ -50,7 +60,7 @@ function addstars() {
   scene.add(star);
 }
 
-Array(300).fill().forEach(addstars);
+Array(200).fill().forEach(addstars);
 
 function moveCamera() {
   const r = document.body.getBoundingClientRect().bottom;
@@ -63,11 +73,15 @@ document.body.onscroll = moveCamera;
 function animate() {
   requestAnimationFrame(animate);
 
-  scene.rotation.z += 0.0001;
+  scene.rotation.z += 0.0002;
 
-  center.rotation.x += 0.001;
-  center.rotation.y += 0.001;
-  center.rotation.z += 0.001;
+  center.rotation.x += 0.005;
+  center.rotation.y += 0.005;
+  center.rotation.z += 0.005;
+
+  centerCube.rotation.x -= 0.005;
+  centerCube.rotation.y -= 0.005;
+  centerCube.rotation.z -= 0.005;
 
   controls.update;
   renderer.render(scene, camera);
